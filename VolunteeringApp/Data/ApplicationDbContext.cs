6d6164e3f2333fdd4b3af03f19917e78cf0766e6
Vassiliki Karagianniki <78561945+vassilikikrg@users.noreply.github.com>
2023-12-08 11:55:50 +0200

    Role seeding ( Organizations and CItizens)

--- VolunteeringApp/Data/ApplicationDbContext.cs
+++ VolunteeringApp/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using VolunteeringApp.Models;
 
@@ -7,5 +8,18 @@ namespace VolunteeringApp.Data
     public class ApplicationDbContext : IdentityDbContext<AppIdentityUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            SeedRoles(builder);
+        }
+
+        private static void SeedRoles(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<IdentityRole>().HasData(
+                new IdentityRole() { Name = "Organization", ConcurrencyStamp = "1",NormalizedName="ORGANIZATION"},
+                new IdentityRole() { Name = "Citizen", ConcurrencyStamp = "2", NormalizedName = "CITIZEN" }
+                );
+        }
     }
 }
---
+++ VolunteeringApp/Migrations/20231208095002_RoleSeeding.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace VolunteeringApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class RoleSeeding : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "0baefd1f-3140-4da8-816a-f4bedab3ef71", "1", "Organization", "ORGANIZATION" },
+                    { "33a14a6c-9c12-48cc-9d94-e6afec10ff49", "2", "Citizen", "CITIZEN" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "0baefd1f-3140-4da8-816a-f4bedab3ef71");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "33a14a6c-9c12-48cc-9d94-e6afec10ff49");
+        }
+    }
+}
