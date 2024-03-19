6937b01cc3b9654515b3d127218a4ecb8c2add1b
Vassiliki Karagianniki <78561945+vassilikikrg@users.noreply.github.com>
2024-03-19 22:28:58 +0200

    send message to group only

--- VolunteeringApp/Hubs/ChatHub.cs
+++ VolunteeringApp/Hubs/ChatHub.cs
@@ -1,12 +1,20 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using VolunteeringApp.Models;
+using VolunteeringApp.Models.Chat;
 
 namespace VolunteeringApp.Hubs
 {
     public class ChatHub : Hub
     {
-        public async Task SendMessage(string user, string message)
+        public async Task SendMessage(string roomName, string message)
         {
-            await Clients.All.SendAsync("ReceiveMessage", user, message);
+            var senderUserId = Context.User.Identity.Name; // get sender's Id
+            await Clients.Group(roomName).SendAsync("ReceiveMessage", senderUserId, message);
+        }
+
+        public async Task JoinPrivateChatRoom(ChatRoom chatRoom)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId,chatRoom.roomName);
         }
     }
 }
---
+++ VolunteeringApp/Models/Chat/ChatRoom.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace VolunteeringApp.Models.Chat
+{
+    public class ChatRoom
+    {
+        public List<IdentityUser>  users { get; set; }
+        public string roomName { get; set; } = string.Empty;
+        
+    }
+}
