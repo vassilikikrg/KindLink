47de2c1d287e986b98db1495085fe8c9780dc1ca
Vassiliki Karagianniki <78561945+vassilikikrg@users.noreply.github.com>
2024-03-25 23:38:20 +0200

    display both sent and received messages
    -Make a template
    - create a scrollable container
    - move text box and button to bottom of screen
    - FIX MULTIPLE CONNECTIONS!!

--- VolunteeringApp/wwwroot/js/chat.js
+++ VolunteeringApp/wwwroot/js/chat.js
@@ -8,12 +8,15 @@ document.getElementById("sendButton").disabled = true; // disable the send butto
 connection.on("ReceiveMessage", function (id,username, message) {
     var activeUserId = receiverId; // get value from input field
     if (id === activeUserId) { // if message received belongs to the open chat
-        $.ajax({
-            url: '/Chat/RenderMessage',
-            data: { userId: id, userName: username, message: message }
-        }).done(function (result) {
-            document.getElementById("messagesList").insertAdjacentHTML('beforeend', result);
-        });
+
+        var receivedMessageHtml = '<div class="row">' +
+            '<div class="col-5 alert alert-secondary text-break" role="alert">' +
+            message +
+            '<p class="mb-0 text-end fs-6 fst-italic">@' +
+            username +
+            '</p>' +
+            '</div> <div class="col-7"></div></div>';
+        document.getElementById("messagesList").insertAdjacentHTML('beforeend', receivedMessageHtml); // append the message to the list
     } else { // display badge to indicate unread messages
         console.log("message was from other user");
         $('.list-group-item[data-userid="' + id + '"] .badge').text('New message');
@@ -29,10 +32,22 @@ connection.start().then(function () {
 
 document.getElementById("sendButton").addEventListener("click", function (event) {
     var message = document.getElementById("messageInput").value;
-    connection.invoke("SendMessageToUser", receiverId ,message).catch(function (err) {
+    connection.invoke("SendMessageToUser", receiverId, message)
+        .then(function () { // message was successfully sent
+            document.getElementById("messageInput").value = ''; //clear message input field
+            var messageHtml = '<div class="row">' +
+                '<div class="col-7"></div>' +
+                '<div class="col-5 alert alert-primary text-break" role="alert">' +
+                message +
+                '<p class="mb-0 text-end fs-6 fst-italic">@' + "me" + '</p>' +
+                '</div></div>';
+            document.getElementById("messagesList").insertAdjacentHTML('beforeend', messageHtml); 
+
+        })
+        .catch(function (err) { // message not sent
         return console.error(err.toString());
-    });
-    document.getElementById("messageInput").value = ''; //clear message input field
+        });
+
     
     event.preventDefault();
 });
