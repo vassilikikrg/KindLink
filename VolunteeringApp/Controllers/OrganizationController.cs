ba5fb77ece994da0a83ff0274e0a39c951523b8c
Vassiliki Karagianniki <78561945+vassilikikrg@users.noreply.github.com>
2024-04-01 16:08:50 +0300

    simple controller that displays all organizations and citizen can click to view details or to send them a message

---
+++ VolunteeringApp/Controllers/OrganizationController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using VolunteeringApp.Data;
+
+namespace VolunteeringApp.Controllers
+{
+    public class OrganizationController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public OrganizationController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Organization
+        public async Task<IActionResult> Index()
+        {
+
+            return View(await _context.Organizations.ToListAsync());
+        }
+
+        // GET: Organization/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var organizationViewModel = await _context.Organizations
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (organizationViewModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(organizationViewModel);
+        }
+
+       
+
+        private bool OrganizationViewModelExists(string id)
+        {
+            return _context.Organizations.Any(e => e.Id == id);
+        }
+    }
+}
