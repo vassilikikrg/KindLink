33ef72debe3bbd8a0373d8c9d924dc74e38a18c4
Vassiliki Karagianniki <78561945+vassilikikrg@users.noreply.github.com>
2024-04-07 17:54:52 +0300

    add search fields for filtering organizations' results

--- VolunteeringApp/Controllers/OrganizationController.cs
+++ VolunteeringApp/Controllers/OrganizationController.cs
@@ -1,23 +1,45 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using VolunteeringApp.Data;
+using VolunteeringApp.Models.Identity;
 
 namespace VolunteeringApp.Controllers
 {
     public class OrganizationController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private readonly UserManager<AppIdentityUser> _userManager;
 
-        public OrganizationController(ApplicationDbContext context)
+        public OrganizationController(ApplicationDbContext context,UserManager<AppIdentityUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
 
         // GET: Organization
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(string searchString)
         {
+            if (_context.Organizations == null)
+            {
+                return Problem("Entity set 'MvcMovieContext.Organizations'  is null.");
+            }
 
-            return View(await _context.Organizations.ToListAsync());
+            var organizations = from o in _context.Organizations
+                                select o;
+            // Apply search filter
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                organizations = organizations.Where(s => s.OfficialName!.Contains(searchString) || s.UserName!.Contains(searchString));
+            }
+
+            if(User.Identity.IsAuthenticated && User.IsInRole("Organization"))
+            {        
+                // Exclude the current user from the organizations list
+                string currentUserId = _userManager.GetUserId(User);
+                organizations = organizations.Where(s=>s.Id!= currentUserId);
+            }
+            return View(await organizations.ToListAsync());
         }
 
         // GET: Organization/Details/5
@@ -38,7 +60,7 @@ namespace VolunteeringApp.Controllers
             return View(organizationViewModel);
         }
 
-       
+
 
         private bool OrganizationExists(string id)
         {
