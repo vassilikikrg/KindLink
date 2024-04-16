b812b1426fdbb0d908b5d27208fa8c1f799ba472
Vassiliki Karagianniki <78561945+vassilikikrg@users.noreply.github.com>
2024-04-16 17:20:39 +0300

    Starters of edit and delete for organizations

--- VolunteeringApp/Controllers/OrganizationController.cs
+++ VolunteeringApp/Controllers/OrganizationController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using VolunteeringApp.Data;
@@ -12,11 +13,13 @@ namespace VolunteeringApp.Controllers
     {
         private readonly ApplicationDbContext _context;
         private readonly UserManager<AppIdentityUser> _userManager;
+        private readonly SignInManager<AppIdentityUser> _signInManager;
 
-        public OrganizationController(ApplicationDbContext context,UserManager<AppIdentityUser> userManager)
+        public OrganizationController(ApplicationDbContext context,UserManager<AppIdentityUser> userManager, SignInManager<AppIdentityUser> signInManager)
         {
             _context = context;
             _userManager = userManager;
+            _signInManager = signInManager;
         }
 
         // GET: Organization
@@ -74,17 +77,217 @@ namespace VolunteeringApp.Controllers
         {
             if (id == null)
             {
-                return NotFound();
+                // If id is null, try to get the ID from the current user
+                id = _userManager.GetUserId(User);
+
+                if (id == null)
+                {
+                    // If the ID is still null, return NotFound
+                    return NotFound();
+                }
             }
 
-            var organizationViewModel = await _context.Organizations
+            var organization = await _context.Organizations
                 .FirstOrDefaultAsync(m => m.Id == id);
-            if (organizationViewModel == null)
+            if (organization == null)
             {
                 return NotFound();
             }
 
-            return View(organizationViewModel);
+            return View(organization);
+        }
+
+        [Authorize(Roles = "Organization")]
+        // GET: Organization/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            // Retrieve the ID of the currently logged-in user
+            var loggedInUserId = _userManager.GetUserId(User);
+
+            // Retrieve the profile from the database
+            var profile = await _context.Organizations.FindAsync(id);
+
+            // Check if the profile exists 
+            if (profile == null)
+            {
+                // If the profile doesn't exist return a 404 Not Found
+                return NotFound();
+            }
+            // Check if the logged-in user is the owner
+            if (profile.Id != loggedInUserId)
+            {   // If the logged-in user is not the owner return an access denied response
+                return Forbid();
+            }
+
+            // If the logged-in user is the owner, allow them to edit the profile
+            return View(profile);
+        }
+
+        // POST: Organization/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Organization")]
+        public async Task<IActionResult> Edit(string id, [Bind("Id,UserName,Email,OfficialName,Phone,Website,OrganizationType,Description")] Organization organization)
+        {
+            if (id != organization.Id)
+            {
+                return NotFound();
+            }
+
+            // Retrieve the ID of the currently logged-in user
+            var loggedInUserId = _userManager.GetUserId(User);
+
+            // Check if the logged-in user is the owner of the profile and can apply changes
+            if (organization.Id != loggedInUserId)
+            {
+                // If the logged-in user is not the owner, return an access denied response
+                return Forbid();
+            }
+
+            // Check if the entered username is already in use by another user
+            var existingUserWithSameUsername = await _userManager.FindByNameAsync(organization.UserName);
+            if (existingUserWithSameUsername != null && existingUserWithSameUsername.Id != organization.Id)
+            {
+                ModelState.AddModelError("UserName", "The username is already in use by another user");
+            }
+
+            // Check if the entered email is already in use
+            var existingUserWithSameEmail = await _userManager.FindByEmailAsync(organization.Email);
+            if (existingUserWithSameEmail != null && existingUserWithSameEmail.Id != organization.Id)
+            {
+                ModelState.AddModelError("Email", "The email is already in use");
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    // Retrieve the profile from the database
+                    var existingOrganization = await _context.Organizations.FindAsync(id);
+
+                    // Check if the profile exists
+                    if (existingOrganization == null)
+                    {
+                        return NotFound();
+                    }
+
+                    // Update the existing Organization entity with the new values
+                    existingOrganization.UserName = organization.UserName;
+                    existingOrganization.Email = organization.Email;
+                    existingOrganization.OfficialName = organization.OfficialName;
+                    existingOrganization.Phone = organization.Phone;
+                    existingOrganization.Website=organization.Website;
+                    existingOrganization.OrganizationType = organization.OrganizationType;
+                    existingOrganization.Description = organization.Description;
+
+                    // Update the entity in the database
+                    _context.Update(existingOrganization);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!OrganizationExists(organization.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction("Details", new { id = organization.Id });
+            }
+            return View(organization);
+        }
+
+
+
+        // GET: Organization/Delete/5
+        [Authorize(Roles = "Organization")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var organization = await _context.Organizations
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (organization == null)
+            {
+                return NotFound();
+            }
+
+            return View(organization);
+        }
+
+        // POST: Organization/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        [Authorize]
+        [Authorize(Roles = "Organization")]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _userManager.FindByIdAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            // Retrieve the ID of the currently logged-in user
+            var loggedInUserId = _userManager.GetUserId(User);
+
+            // Check if the logged-in user is the owner of the profile and can delete the account
+            if (id != loggedInUserId)
+            {
+                // If the logged-in user is not the owner, return an access denied response
+                return Forbid();
+            }
+
+            var rolesForUser = await _userManager.GetRolesAsync(user);
+            var logins = await _userManager.GetLoginsAsync(user);
+            IdentityResult result = IdentityResult.Success;
+
+            foreach (var login in logins)
+            {
+                result = await _userManager.RemoveLoginAsync(user, login.LoginProvider, login.ProviderKey);
+                if (result != IdentityResult.Success)
+                    break;
+            }
+            if (rolesForUser.Count > 0)
+            {
+                foreach (var role in rolesForUser)
+                {
+                    result = await _userManager.RemoveFromRoleAsync(user, role);
+                    if (!result.Succeeded)
+                    {
+                        // Handle role removal failure
+                        break;
+                    }
+                }
+            }
+
+            var resultDelete = await _userManager.DeleteAsync(user);
+            if (resultDelete.Succeeded)
+            {
+                await _signInManager.SignOutAsync();
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                // Handle user deletion failure
+                return RedirectToAction("Organization", "Delete");
+            }
         }
 
 
